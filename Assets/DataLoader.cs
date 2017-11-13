3dd7d16c8a29ed55540c2a573431ad5ec875d2e3
AkshayPakhle10 <akshay.pakhle888@gmail.com>
2017-11-13 15:41:00 +0530

    Initial Commit

---
+++ Assets/DataLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+
+	public string[] users;
+	
+	IEnumerator Start () {
+		WWW userData = new WWW("http://localhost/Test/UserData.php");
+		yield return userData;
+		string userDataString = userData.text;
+		print (userDataString);
+		users = userDataString.Split(';');
+		print (GetDataValue(users[0], "Points:"));
+	}
+
+	string GetDataValue(string data, string index){
+		string value = data.Substring(data.IndexOf(index) + index.Length);
+		if(value.Contains("|"))value = value.Remove(value.IndexOf("|"));
+		return value;
+	}
+	
+	
+}
---
+++ Assets/GunDataInsert.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDataInsert : MonoBehaviour {
+
+	public string inputName;
+	public string inputClipSize;
+	public string inputDamage;
+
+	string CreateGunUrl = "http://localhost/Test/InsertGunsData.php";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) CreateGun(inputName,inputClipSize,inputDamage);
+	}
+
+	public void CreateGun(string name,string clip_size,string damage){
+		WWWForm form = new WWWForm();
+		form.AddField("gun_name_post",name);
+		form.AddField("gun_clip_size_post",clip_size);
+		form.AddField("gun_damage_post",damage);
+
+		WWW web = new WWW(CreateGunUrl,form);
+
+	}
+}
